8272cbaa1739944b7f07a365c39b29ec689b6b74
김지환 <kjhwan0802@naver.com>
2020-10-18 15:16:37 +0900

    node.js login with mysql data

--- web/main.js
+++ web/main.js
@@ -1,33 +1,67 @@
-var fs = require("fs");
-var server = require('http');
+var fs = require('fs');
+var http = require('http');
+var qs = require('querystring');
+
+var sql = require('./mysql.js');
 
 
-fs.writeFile("inputData.txt", "inputinputinput", "utf8", function(err) {
-	if(err) {
-		console.log(err);
-	} else {
-		console.log("inputDate 쓰기 완료\n")
+http.createServer(function(req, res) {					//http 서버 생성.
+
+	var readHtml = "web/site.html";
+	fs.readFile(readHtml, "utf8", function(err, data) {	//web/index.html를 읽어와서 data에 저장.	
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("******" + readHtml + "읽음*********\n");
+
+			res.writeHead(200, {'Content-Type' : 'text/html'});	//연결되면 end("내용")에서 내용이 html 형식으로 전달됨.
+			res.end(data);
+		}
+	});
+
+
+	
+	//html form 에서 post로 보낸 데이터 받기
+	if(req.method == 'POST') {
+		var body = '';			//너무 많은 데이터가 들어와도 받을 수 있도록
+
+		req.on('data', function(data) {
+			body += data;
+		});
+
+
+		var postData = null;
+		req.on('end', function() {
+			postData = qs.parse(body);
+
+			var inputData = [postData.inputName, postData.inputId, postData.inputPw];
+
+			console.log(postData);
+
+
+
+			sql.db.query('INSERT INTO member (Name, ID, PW) VALUES (?, ?, ?);', inputData, function(err, rows, fields) {
+				if(err) {
+					console.log(err);
+				} else {
+					console.log('********mysql에 입력 완료*******\n' + rows);
+				}
+			});
+
+			
+		});
+
 	}
-})
 
-fs.readFile("inputData.txt", "utf8", function(err, data) {
-	if(err) {
-		console.log(err);
-	} else {
-		console.log("inputData 읽음\n" + data);
-	}
+
+}).listen(3000, function() {	//포트 3000
+	console.log('Server is running at http://localhost:3000/\n');
 });
 
 
 
-server.createServer(function(req, res) {
-	res.writeHead(200, { 
-    	'Content-Type' : 'text/html' 
-  	});
-	fs.createReadStream("./site.html", {encoding:'utf8'}).pipe(res);
-	
-}).listen(3000);
 
-console.log('Server is running at goorm.io');
+
+
 
 
