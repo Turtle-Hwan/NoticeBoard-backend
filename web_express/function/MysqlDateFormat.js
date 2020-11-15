1694a50a3c853e3bfc782bbea000bc3918cf4d46
김지환 <kjhwan0802@naver.com>
2020-11-16 00:36:43 +0900

    change date format js to mysql

---
+++ web_express/function/MysqlDateFormat.js
@@ -0,0 +1,22 @@
+
+exports.MysqlDateFormat = function(date) {
+    let year = date.getFullYear();
+
+    let month = 1 + date.getMonth();
+    month = month >= 10 ? month : '0' + month;
+
+    let day = date.getDate();
+    day = day >= 10 ? day : '0' + day;
+
+    let hh = date.getHours();
+    hh = hh >= 10 ? hh : '0' + hh;
+
+    let mm = date.getMinutes();
+    mm = mm >= 10 ? mm : '0' + mm;
+
+    let ss = date.getSeconds();
+    ss = ss >= 10 ? ss : '0' + ss;
+
+    
+    return year + '-' + ss + '-' + ss + ' ' + ss + ':' + ss + ':' + ss;
+}
